8d7eb8429793447c9bd5df3d9c9771f0b04df896
VitaliiVoloshin <vitalii.voloshin@mytona.com>
2019-12-09 18:05:44 +0900

    commit

---
+++ ALC/Assets/Scripts/MenuController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject MainMenu;
+    public GameObject Options;
+
+    public void PlayGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void GameOptions() {
+        
+    }
+    public void GameQuit() {
+        Application.Quit();
+    }
+
+}
