d8ee97fb081f5931565f8570da929aece0279034
VitaliiVoloshin <vitalii.voloshin@mytona.com>
2019-12-10 13:24:49 +0900

    Dinner commit

--- ALC/Assets/Scripts/MenuController.cs
+++ ALC/Assets/Scripts/MenuController.cs
@@ -5,16 +5,41 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
-    public GameObject MainMenu;
-    public GameObject Options;
+    public GameObject mainMenu;
+    public GameObject options;
+    private bool optionState;
+
+    private void Start()
+    {
+        optionState = false;
+    }
+
+    private void Update()
+    {
+        if (optionState)
+        {
+            options.SetActive(true);
+            mainMenu.SetActive(false);
+        }
+        else
+        {
+            options.SetActive(false);
+            mainMenu.SetActive(true);
+        }
+    }
 
     public void PlayGame() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    public void GameOptions() {
-        
+    public void BackToMainMenu() {
+        optionState = false;
     }
+
+    public void GameOptions() {
+        optionState = true;
+    }
+
     public void GameQuit() {
         Application.Quit();
     }
